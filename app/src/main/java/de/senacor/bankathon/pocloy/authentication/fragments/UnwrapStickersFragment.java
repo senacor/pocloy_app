1b445341038c77dc39e2748578482a4b85c11d66
Daniel <Daniel.Siebert@senacor.com>
2018-08-28 21:19:08 +0200

    Exclude unwrap all button

--- app/src/main/java/de/senacor/bankathon/pocloy/authentication/fragments/UnwrapStickersFragment.java
+++ app/src/main/java/de/senacor/bankathon/pocloy/authentication/fragments/UnwrapStickersFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import java.util.List;
 import butterknife.BindView;
